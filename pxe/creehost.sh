9ced221ab19c0dd297fc8e91265c9ffea431cc9c
knight75 <kurgan231@gmail.com>
2011-05-22 19:38:42 +0200

    script pxe new version still things to modify....

--- pxe/creehost.sh
+++ pxe/creehost.sh
@@ -3,7 +3,7 @@ Menu()
 {
  local -a menu fonc
  local titre nbchoix
- DHCPFILE=/opt/scripts/dhcp/dhcpd.conf
+ DHCPFILE=/opt/scripts/pxe/dhcp/dhcpd.conf
       # Constitution du menu
       if [[ $(( $# % 1 )) -ne 0 ]] ; then
          echo "$0 - Menu invalide" >&2
@@ -40,13 +40,18 @@ Menu()
     #------------------------------------------------
     AddHost()
     {
-       echo -e "\n***\n*** AddHost\n***\n"
-                 cd /opt/scripts/dhcp
-                 echo "quel est le hostname?\n"
+                 cd /opt/scripts/pxe/dhcp
+                 echo "quel est le hostname?"
                  read hostname
 
-          cat $DHCPFILE | awk '{print "host " $1 " {\n    hardware ethernet " $2 ";\n    fixed-address " $3 ";\n    if substring (option vendor-class-identifier, 0, 9) = \"PXEClient\" {\n      filename \"PXEFILE\";\n    } else if substring (option vendor-class-identifier, 0, 9) = \"Etherboot\" {\n      filename \"ETHERBOOTFILE\";\n      option vendor-encapsulated-options 3c:09:45:74:68:65:72:62:6f:6f:74:ff;\n    }\n}\n"}'
-#sed "s/PXEFILE/$PXEFILE/" | sed "s/ETHERBOOTFILE/$ETHERBOOTFILE/"
+                 echo "saisissez la macadress?"
+                 read macaddress
+
+                 echo "quel est l'adresse IP?"
+                 read IP
+
+                 sed "s/<hostname>/$hostname/g" | sed "s/<macaddress>/$macaddress/g" $DHCPFILE
+                 exit
     }
     #------------------------------------------------
     # AddHosts - Ajout de plusieurs hosts
