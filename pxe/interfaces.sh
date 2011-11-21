122b362f609f53551a5750194aba3082d964a23d
knight75 <kurgan231@gmail.com>
2011-11-21 17:18:04 +0100

    scripts interfaces when pxe installation ends, give to host wich must have static ip there ips 2 versions interfaces.sh when system language is franch interfaces_en.sh: when its english not the best way to do taht, but not the worst...

---
+++ pxe/interfaces.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+interfaces=/etc/network/interfaces
+ip=`ifconfig eth0 |grep Masque|cut -d ':' -f2| cut -d ' ' -f1`
+mask=`ifconfig eth0 |grep Masque|cut -d ':' -f4| cut -d ' ' -f1`
+bcast=` ifconfig eth0 |grep Masque|cut -d ':' -f3| cut -d ' ' -f1`
+gw=`route -n |tail -n1|cut -d' ' -f10`
+net=`route -n | tail -n2 |awk '{print $1}' | grep 178`
+
+sed -i 's/dhcp/static/g' $interfaces
+
+echo "  address $ip" >> $interfaces
+echo "  netmask $mask" >> $interfaces
+echo "  network $net" >> $interfaces
+echo "  broadcast $bcast" >> $interfaces
+echo "  gateway $gw" >> $interfaces
+
+echo "  # dns-* options are implemented by the resolvconf package, if installed" >> $interfaces                                                                                                                
+echo "  dns-nameservers $nameservers" >> $interfaces
+echo "  dns-search $search" >> $interfaces
