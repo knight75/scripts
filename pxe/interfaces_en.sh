dcc83086c0a4fea5428fefa35f0da4461fbd5c2b
knight75 <kurgan231@gmail.com>
2012-03-16 07:36:02 +0100

    scripts pxe debian pxe postinstall scripts french and us version

--- pxe/interfaces_en.sh
+++ pxe/interfaces_en.sh
@@ -3,9 +3,9 @@
 interfaces=/etc/network/interfaces
 ip=`ifconfig eth0 |grep Mask|cut -d ':' -f2| cut -d ' ' -f1`
 mask=`ifconfig eth0 |grep Mask|cut -d ':' -f4| cut -d ' ' -f1`
-bcast=` ifconfig eth0 |grep Mask|cut -d ':' -f3| cut -d ' ' -f1`
+bcast=`route -n | tail -n2 |awk '{print $1}' | grep 10 | sed 's/.0$/.255/'`
 gw=`route -n |tail -n1|cut -d' ' -f10`
-net=`route -n | tail -n2 |awk '{print $1}' | grep 178`
+net=`route -n | tail -n2 |awk '{print $1}' | grep 10`
 
 sed -i 's/dhcp/static/g' $interfaces
 
@@ -16,5 +16,5 @@ echo "  broadcast $bcast" >> $interfaces
 echo "  gateway $gw" >> $interfaces
 
 echo "  # dns-* options are implemented by the resolvconf package, if installed" >> $interfaces                                                                                                                
-echo "  dns-nameservers $namservers" >> $interfaces
-echo "  dns-search $search" >> $interfaces
+echo "  dns-nameservers serveraddress serveraddress" >> $interfaces
+echo "  dns-search domain" >> $interfaces
