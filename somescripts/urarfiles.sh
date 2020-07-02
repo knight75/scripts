400cfa11e23de376898f9ce5b583c147a1c51c02
Knight75 <kurgan231@gmail.com>
2020-07-02 12:13:09 +0200

    V1 works complete

--- somescripts/urarfiles.sh
+++ somescripts/urarfiles.sh
@@ -6,11 +6,12 @@ source .params.txt
 for f in $rarfile
     do
       printf "Extracting $f\n"
+      filename=$(echo $f | sed "s/.part1.rar//g")
 
       unrar e $f $rardir; vstat=$?
 
       case $vstat in
-          0) printf "moving files to $olddir\n"; mv $f $olddir ;;
+          0) printf "moving files to $olddir\n"; mv $filename* $olddir ;;
           1) printf '%s\n' "Oups something went wrong\nCommand exited with non-zero"; exit 1;;
           *) printf 'ignoring exit statusi: $vstat'; exit 1;;
       esac
